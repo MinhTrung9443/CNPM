6fa8435f0c221f3bf743164df52054042d0bfe4b
MinhTrung9443 <minhtrungbttv@gmail.com>
2024-11-25 11:39:37 +0700

    merge xong, còn lỗi logic

--- src/main/java/com/cnpm/controller/ChatController.java
+++ src/main/java/com/cnpm/controller/ChatController.java
@@ -13,7 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Controller
+@Controller("/employee")
 public class ChatController {
 
 	@GetMapping("/chat/{SessionInfoname}")
