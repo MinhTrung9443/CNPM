254f65bfdea3667dca837017ab2820e95854d76b
MinhTrung9443 <minhtrungbttv@gmail.com>
2024-11-25 08:17:13 +0700

    fix

--- src/main/java/com/cnpm/controller/owner/OwnerController.java
+++ src/main/java/com/cnpm/controller/owner/OwnerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/owner")
 public class OwnerController {
 
     @GetMapping({"", "/index", "/index.html"})
